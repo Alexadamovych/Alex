66ddb2503034b0c2c913eb29e72768474056d979
1013 <jekakrv209@gmail.com>
2023-10-31 16:58:02 +0200

    Lesson3 teory Create class Human Create class Auto

---
+++ lesson 3/Sims.py
@@ -0,0 +1,19 @@
+import random
+
+class Human:
+    def __init__(self, name="Human",
+             job=None, home=None,
+             car=None):
+       self.name = name
+       self.money = 100
+       self.gladness = 50
+       self.satiety = 50
+       self.job = job
+
+    def to_repair(self):
+        pass
+
+    class Student:
+        print("Hi")
+
+    first_student = Student()
