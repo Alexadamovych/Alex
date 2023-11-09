5f0750b6535f38604dc11a226e45264560599b92
1013 <jekakrv209@gmail.com>
2023-11-09 16:16:51 +0200

    Lesson6 CreateExeptions create class BuildingError

--- lesson 3/Sims.py
+++ lesson 3/Sims.py
@@ -1,19 +1,25 @@
 import random
 
 class Human:
-    def __init__(self, name="Human",
-             job=None, home=None,
-             car=None):
-       self.name = name
-       self.money = 100
-       self.gladness = 50
-       self.satiety = 50
-       self.job = job
+    def __init__(self, name="Human", job=None,
+                home=None, car=None):
+        self.name = name
+        self.money = 100
+        self.gladness = 50
+        self.satiety = 50
+        self.job = job
+        self.car = car
+        self.home = home
 
-    def to_repair(self):
-        pass
-
-    class Student:
-        print("Hi")
-
-    first_student = Student()
+    def get_home(self):
+        self.home = House()
+    def get_car(self):
+        self.car = Auto(brands_of_car)
+    def get_job(self):
+        if self.car.drive():
+                pass
+            else:
+                self.to_repair()
+                return
+            self.job = Job(job_list)
+        
---
+++ lesson 4/teory.py
@@ -0,0 +1,43 @@
+# class Parent:
+#   pass
+
+# class Child(Parent):
+#    pass
+
+# class Human:
+#     class Child 170
+#
+#
+# class Student(Human):
+#     solary =0
+#
+#
+# class Worker(Human):
+#     solary = 100
+#
+#
+# nick = Student()
+# ann = Worker()
+# print(nick.height)
+# print(ann.height)
+#
+# print(nick.salory)
+# print(ann.salory)
+#
+# class Granparent:
+#     height = 170
+#     satiety = 100
+#     age = 60
+#
+# class Child(Parent)
+#     height = 50
+#     def __init__(self):
+#
+# class Child
+#     def __init__(self):
+
+class Granparent:
+    def about(self):
+        print("I am Grandparent")
+    def about_myself(self):
+
